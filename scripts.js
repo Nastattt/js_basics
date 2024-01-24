87e64b2605133518ae7794a5b7e7dc3ed1b9e287
nastya rudko <ian4iv@yandex.ru>
2024-01-24 11:44:12 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,124 @@
+// let a = 12,
+//     b = 45,
+//     c = 45;
+// // console.log(12,15);
+// console.log(12,15);
+// let d = 15>19
+
+// let user = {
+//     "name":"Vasya",
+//     "age":15,
+//     "email":"aufhnm@bhivj.ru"
+// }
+// console.log(user["name"])
+
+// prompt("Enter your name","Vastya")
+// console.log("12"*"15");
+
+// console.log(++a);
+// console.log(a++);
+
+// дз
+
+// prompt("Enter your name","Я javaScript")
+
+// alert("Я javaScript")
+// let message = prompt("Введите имя", 'massage');
+// console.log(message);
+
+// let num = 3
+// alert(num)
+
+// let a = 10
+// let b = 2
+// alert(a+=b)
+// alert(a-=b)
+// alert(a*=b)
+// alert(a/=b)
+
+// let c = 15
+// let d = 2
+
+// let result = c+=d
+// alert(result)
+
+// let a = 10
+// let b = 2
+// let c = 5
+
+// alert(a+=b+=c)
+
+// let a = 17 
+// let b = 10
+// let c = a-=b
+// let d = 7
+// let result = c+=d
+// alert(result) 
+
+// let str = "Привет,Мир!"
+// alert(str)
+
+
+// var str1 = 'Привет, ';
+// var str2 = 'Мир!';
+
+// var result = str1 + str2;
+// console.log(result);
+
+
+// let name = "Nastya";
+// console.log(`Привет, ${name}!`);
+
+
+// let age = "17";
+// console.log(`Мне ${age} лет`);
+
+// let name = prompt('Ваше имя');
+// alert(`Ваше имя ${name}`);
+
+// let number = prompt('Введите число');
+// alert(number*number)
+
+// let str = 'abcde';
+
+// console.log(str[0]);
+// console.log(str[2]);
+// console.log(str[4]);
+
+// let num = '12345';
+// console.log(num[0] *let secondsInHour = 60 * 60;
+
+
+// let secondsInHour = 60 * 60;
+// let secondsInDay = secondsInHour * 24;
+// let secondsInMonth = secondsInDay * 30;
+
+// console.log('Секунд в часе: ' + secondsInHour);
+// console.log('Секунд в сутках: ' + secondsInDay);
+// console.log('Секунд в месяце: ' + secondsInMonth);
+
+
+// let a = 2
+// console.log(a*a)
+
+// let now = new Date();
+
+// let hour = now.getHours();
+// let minute = now.getMinutes();
+// let second = now.getSeconds();
+
+// console.log(`${hour}:${minute}:${second}`);
+
+// var num = 47;
+// num += 7;
+// num -= 18;
+// num * 10;
+// num /= 15;
+// alert(num);
+
+// var num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num)
+
